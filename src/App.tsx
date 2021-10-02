e2176d65ed87407a88330f0ebb256709f0d41913
Misol Goh <melancholy8914@gmail.com>
2021-10-03 01:08:26 +0200

    Add react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { Typography, Divider, Grid } from '@mui/material';
 
 import LoginForm from './containers/LoginForm';
 
+const queryClient = new QueryClient();
+
 function App() {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+
   return (
-    <div>
-      <header>
-        <Grid container alignItems='center' justifyContent='center'>
-          <Typography variant="h1">Quiz Time</Typography>
-        </Grid>
-      </header>
-      <Divider sx={{ my: 1 }} />
-      <section style={{ minHeight: '50vh' }}>
-        <Grid container alignItems='center' justifyContent='center'>
-          <Grid item xs={6}>
-            <LoginForm />
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <header>
+          <Grid container alignItems='center' justifyContent='center'>
+            <Typography variant="h1">Quiz Time</Typography>
           </Grid>
-        </Grid>
-      </section>
-    </div>
+        </header>
+        <Divider sx={{ my: 1 }} />
+        <section style={{ minHeight: '50vh' }}>
+          <Grid container alignItems='center' justifyContent='center'>
+            <Grid item xs={6}>
+              {!isLogin && <LoginForm onLogin={() => setIsLogin(true)} />}
+              {isLogin && <Typography variant='body1'>You Logged in!</Typography>}
+            </Grid>
+          </Grid>
+        </section>
+      </div>
+    </QueryClientProvider>
   );
 }
 
