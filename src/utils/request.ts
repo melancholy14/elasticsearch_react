56f3aa0061e8933ca31fe1c247b937106f1ef575
Misol Goh <melancholy8914@gmail.com>
2021-10-03 19:20:26 +0200

    Add authorization if uid is in cookie

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,10 +1,15 @@
+import { getUid } from './cookies';
+
 const apiUrl = 'http://localhost:8080';
 
 async function request(url: string, options?: { method?: 'get' | 'post', headers?: Record<string, any>, data?: Record<string, any> }) {
     const { method = 'get', headers = {}, data } = options || {};
 
+    const uid = getUid();
+
     const updatedHeaders = {
         'Content-Type': 'application/json',
+        [uid ? 'Authorization' : '']: `Basic ${uid}`,
         ...headers
     };
 
@@ -14,7 +19,13 @@ async function request(url: string, options?: { method?: 'get' | 'post', headers
         body: JSON.stringify(data),
     });
 
-    return response.json();
+    const { success, error, data: result } = await response.json();
+
+    if (success) {
+        return result;
+    } else {
+        throw new Error(error);
+    }
 }
 
 export default request;
